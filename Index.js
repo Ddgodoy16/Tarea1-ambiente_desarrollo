6ccce69c143218c5fe2410e44bd34ed1b77660b1
Ddgodoy16 <elvinnu_es25@outlook.es>
2024-04-21 16:34:19 -0500

    Revert "Codigo completo"
    This reverts commit 3dbd7e47b80ff8237b6f45310108a805a92eceab.

--- Index.js
+++
@@ -1,9 +0,0 @@
-var colors = require("colors");
-
-
-function suma(num1, num2, num3) {
-    return num1 + num2 * num3;
-  }
-  
-  console.log("El resultado es:".bgGreen)
-  console.log(suma(5, 3, 4))
